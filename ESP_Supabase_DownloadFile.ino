4a2a0002af77c429b194c86735be296caa4e7200
Ehab Magdy <132620660+EhabMagdyy@users.noreply.github.com>
2025-01-27 06:29:42 +0200

    Refactored to Work with ESP32/ESP8266 & Enhanced

--- ESP_Supabase_DownloadFile.ino
+++ ESP_Supabase_DownloadFile.ino
@@ -1,129 +1,154 @@
-/********************************************************************************************************************
-*****************************    Author  : Ehab Magdy Abdullah                      *********************************
-*****************************    Linkedin: https://www.linkedin.com/in/ehabmagdyy/  *********************************
-*****************************    Youtube : https://www.youtube.com/@EhabMagdyy      *********************************
-*********************************************************************************************************************/
+/*****************************************************************************************************************************
+**********************************    Author  : Ehab Magdy Abdullah                      *************************************
+**********************************    Linkedin: https://www.linkedin.com/in/ehabmagdyy/  *************************************
+**********************************    Youtube : https://www.youtube.com/@EhabMagdyy      *************************************
+******************************************************************************************************************************/
 
 #ifdef ESP32
 #include <WiFi.h>
 #include <HTTPClient.h>
+#define POTENTIOMETER_PIN  35
 #elif ESP8266
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#define POTENTIOMETER_PIN  A0
 #endif
-#include <LittleFS.h>
+#include <ArduinoJson.h>
 
 // Wi-Fi credentials
-#define ssid            "ssid"
-#define password        "password"
+#define SSID               "SSID"
+#define PASSWORD           "PASSWORD"
 
 // Supabase credentials
-#define supabaseFileUrl "supabaseFileUrl"
-#define apiKey          "apiKey"
-
-// File path to save the downloaded file
-#define filePath        "/file.bin"
-
-void connectToWiFi()
-{
-  Serial.print("Connecting to Wi-Fi...");
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("\nConnected to Wi-Fi");
-}
-
-// Download the file from Supabase Storage
-void downloadFile()
-{
-  HTTPClient http;
-  
-#ifdef ESP32
-  http.begin(supabaseFileUrl);
-#elif ESP8266
-  WiFiClientSecure client;
-  client.setInsecure(); // not recommended for production
-  http.begin(client, supabaseFileUrl);
-#endif
-
-  http.addHeader("Authorization", "Bearer " + String(apiKey));
-
-  Serial.println("Downloading file...");
-  int httpCode = http.GET();
-
-  if (httpCode == HTTP_CODE_OK)
-  {
-    File file = LittleFS.open(filePath, "w");
-    if (!file)
-    {
-      Serial.println("Failed to open file for writing");
-      return;
-    }
-
-    http.writeToStream(&file); // Write the response to the file
-    file.close();
-    Serial.println("File downloaded and saved to File System");
-  }
-  else
-  {
-    Serial.printf("Failed to download file, HTTP error code: %d\n", httpCode);
-    Serial.printf("Error message: %s\n", http.errorToString(httpCode).c_str());
-  }
-
-  http.end();
-}
-
-// Read the file from LittleFS and print its contents
-void readFile()
-{
-  // Open file for reading
-  File file = LittleFS.open(filePath, "r");
-  if (!file)
-  {
-    Serial.println("Failed to open file");
-    return;
-  }
-
-  uint8_t byteCount = 0;
-  Serial.println("File content:");
-  while (file.available())
-  {
-    uint8_t byte = file.read();
-    Serial.printf("%02X ", byte);
-
-    byteCount++;
-    if (byteCount % 32 == 0)
-      Serial.println();
-  }
-
-  file.close();
-  Serial.println();
-}
+#define supabaseUrl       "supabaseUrl"
+#define supabaseKey       "supabaseKey"
+#define tableName         "tableName"
 
 void setup()
 {
   Serial.begin(115200);
 
-  // Initialize LittleFS
-  if (!LittleFS.begin())
+  // Connect to Wi-Fi
+  WiFi.begin(SSID, PASSWORD);
+  Serial.print("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED)
   {
-    Serial.println("Failed to mount LittleFS file system");
-    return;
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("\nConnected to WiFi\n");
+}
+
+void loop()
+{
+  sendData();         // Update Record of id=1
+  getData();          // Read Record of id=1
+  delay(5000);        // Wait 5 Seconds
+}
+
+void sendData()
+{
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("WiFi not connected. Attempting to reconnect...");
+    while (!WiFi.reconnect())
+    {
+      Serial.println("Reconnecting to WiFi...");
+      delay(500);
+    }
+    Serial.println("WiFi reconnected.");
+  }
+  // Create a JSON object with your data
+  StaticJsonDocument<200> jsonDoc;
+  // Reading Potentiometer value
+  jsonDoc["potentiometer"] = analogRead(POTENTIOMETER_PIN);
+
+  // Serialize JSON to a string
+  String jsonString;
+  serializeJson(jsonDoc, jsonString);
+
+  // Update an existing row | Send HTTP PATCH request to Supabase
+#ifdef ESP32
+  HTTPClient http;
+  String endpoint = String(supabaseUrl) + "/rest/v1/" + tableName + "?id=eq.1"; // Update row with id=1
+  http.begin(endpoint);
+#elif ESP8266
+  WiFiClientSecure client;
+  client.setInsecure(); // not recommended for production
+  HTTPClient http;
+  String endpoint = String(supabaseUrl) + "/rest/v1/" + tableName + "?id=eq.1"; // Update row with id=1
+  http.begin(client, endpoint);
+#endif
+
+  http.addHeader("Content-Type", "application/json");
+  http.addHeader("apikey", supabaseKey);
+  http.addHeader("Authorization", "Bearer " + String(supabaseKey));
+  // Optional: Return the updated row
+  http.addHeader("Prefer", "return=representation");
+
+  // Update the specified row
+  int httpResponseCode = http.PATCH(jsonString);
+  if (httpResponseCode > 0)
+  {
+    Serial.println("Transmitting:");
+    String response = http.getString();
+    Serial.println("HTTP Response code: " + String(httpResponseCode));
+    Serial.println("Response: " + response);
   }
   else
   {
-    Serial.println("LittleFS mounted successfully");
+    Serial.println("Error in HTTP request");
+    Serial.println("HTTP Response code: " + String(httpResponseCode));
+    String response = http.getString();
+    Serial.println("Response: " + response);
   }
-
-  connectToWiFi();
-  // Download the file from Supabase Storage
-  downloadFile();
-  // Read and print the file contents
-  readFile();
+  http.end();
 }
 
-void loop() {}
+void getData()
+{
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("WiFi not connected. Attempting to reconnect...");
+    while (!WiFi.reconnect())
+    {
+      Serial.println("Reconnecting to WiFi...");
+      delay(500);
+    }
+    Serial.println("WiFi reconnected.");
+  }
+
+  // GET request
+#ifdef ESP32
+  HTTPClient http;
+  String endpoint = String(supabaseUrl) + "/rest/v1/" + tableName + "?id=eq.1"; // Update row with id=1
+  http.begin(endpoint);
+#elif ESP8266
+  WiFiClientSecure client;
+  client.setInsecure(); // not recommended for production
+  HTTPClient http;
+  String endpoint = String(supabaseUrl) + "/rest/v1/" + tableName + "?id=eq.1"; // Update row with id=1
+  http.begin(client, endpoint);
+#endif
+
+  http.addHeader("apikey", supabaseKey);
+  http.addHeader("Authorization", "Bearer " + String(supabaseKey));
+
+  int httpResponseCode = http.GET();
+  if (httpResponseCode > 0)
+  {
+    Serial.println("\nReceiving:");
+    String response = http.getString();
+    Serial.println("HTTP Response code: " + String(httpResponseCode));
+    Serial.println("Response: " + response);
+  }
+  else
+  {
+    Serial.println("Error in HTTP request");
+    Serial.println("HTTP Response code: " + String(httpResponseCode));
+    String response = http.getString();
+    Serial.println("Response: " + response);
+  }
+  http.end();
+  Serial.println("\n============================================\n");
+}
